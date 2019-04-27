e1a86d6f788f80e008a8f13fff4443150deb7a55
MrPiedPiper <35707072+MrPiedPiper@users.noreply.github.com>
2019-04-27 09:48:56 -0700

    Add button for player to interact with

---
+++ Assets/Scripts/Button.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+signal button_activated
+signal button_deactivated
+
+export var linkedObject : PackedScene
+var deactiveSprite : Texture = preload("res://Assets/Sprites/ButtonUp.png")
+var activeSprite : Texture = preload("res://Assets/Sprites/ButtonDown.png")
+
+var collidingWith = []
+
+func _ready():
+	if linkedObject != null:
+		connect("button_activated", linkedObject, "_on_activated")
+		connect("button_deactivated", linkedObject, "_on_deactivated")
+
+func _on_Area2D_area_entered(area):
+	emit_signal("button_activated")
+	collidingWith.append(area)
+	$Sprite.texture = activeSprite
+
+
+func _on_Area2D_area_exited(area):
+	collidingWith.erase(area)
+	if collidingWith.size()==0:
+		emit_signal("button_deactivated")
+		$Sprite.texture = deactiveSprite
