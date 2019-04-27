2f07f3844fdfae25c266b4b16888d5b5015e2bec
MrPiedPiper <35707072+MrPiedPiper@users.noreply.github.com>
2019-04-27 10:34:11 -0700

    Add gate

--- Assets/Scripts/Button.gd
+++ Assets/Scripts/Button.gd
@@ -3,16 +3,18 @@ extends Node2D
 signal button_activated
 signal button_deactivated
 
-export var linkedObject : PackedScene
+export var linkedObjectPath : NodePath
+var linkedObject
 var deactiveSprite : Texture = preload("res://Assets/Sprites/ButtonUp.png")
 var activeSprite : Texture = preload("res://Assets/Sprites/ButtonDown.png")
 
 var collidingWith = []
 
 func _ready():
-	if linkedObject != null:
-		connect("button_activated", linkedObject, "_on_activated")
-		connect("button_deactivated", linkedObject, "_on_deactivated")
+	if linkedObjectPath != null:
+		var linkedObject = get_node(linkedObjectPath)
+		connect("button_activated", linkedObject, "_on_activated", [self])
+		connect("button_deactivated", linkedObject, "_on_deactivated", [self])
 
 func _on_Area2D_area_entered(area):
 	emit_signal("button_activated")
---
+++ Assets/Scripts/Gate.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var deactiveSprite : Texture = preload("res://Assets/Sprites/GateClosed.png")
+var activeSprite : Texture = preload("res://Assets/Sprites/GateOpen.png")
+
+var activeArray = []
+
+func _on_activated(_node):
+	activeArray.append(_node)
+	process_gate()
+	
+func _on_deactivated(_node):
+	activeArray.erase(_node)
+	process_gate()
+	
+func process_gate():
+	print(activeArray.size())
+	if activeArray.size() > 0:
+		$KinematicBody2D/CollisionShape2D.set_deferred("disabled", true)
+		$Sprite.texture = activeSprite
+	else:
+		$KinematicBody2D/CollisionShape2D.set_deferred("disabled", false)
+		$Sprite.texture = deactiveSprite
